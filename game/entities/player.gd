da23bd0e7f75b53d746a45f1009bc9f496b33bc4
Bratah123 <brandonnguyen3301@gmail.com>
2024-02-12 00:23:11 -0800

    FEAT: Add player movement

---
+++ game/entities/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var move_speed: float = 200.0
+
+func handle_input():
+	var move_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = move_direction * move_speed
+
+func _physics_process(delta):
+	handle_input()
+	move_and_slide()
